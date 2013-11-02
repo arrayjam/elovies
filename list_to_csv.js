74d2935a514a8e2737adab7f3dfa17803a9805d0
Yuri Feldman <arrayjam@tbqh.net>
2013-11-03 03:44:17 +1100

    Generate links for omdb, split out name&year

--- list_to_csv.js
+++ list_to_csv.js
@@ -10,19 +10,25 @@ var boundaries = {
 
 fs.readFile("movie_ratings.list", "utf8", function(err, data) {
   var lines = data.split("\n");
-  var ratings = lines.map(function(line) {
+  var ratings = lines
+    .filter(function(line) { return line; })
+    .map(function(line) {
     var distribution = line.slice(boundaries.distribution[0], boundaries.distribution[1]).trim();
     var votes = line.slice(boundaries.votes[0], boundaries.votes[1]).trim();
     var rating = line.slice(boundaries.rating[0], boundaries.rating[1]).trim();
-    var title = line.slice(boundaries.title[0]).trim();
-    var game = title.indexOf("(VG)") !== -1;
-    var tv = title.indexOf("(TV)") !== -1;
-    var tv_movie = title.indexOf("(V)") !== -1;
+    var titleYear = line.slice(boundaries.title[0]).trim();
+    var game = titleYear.indexOf("(VG)") !== -1;
+    var tv = titleYear.indexOf("(TV)") !== -1;
+    var tv_movie = titleYear.indexOf("(V)") !== -1;
+    var match = titleYear.match(/(.+)\(([\d/IVX]+)\)/);
+    var title = match[1].trim();
+    var year = match[2];
     return {
       distribution: distribution,
       votes: +votes,
       rating: +rating,
       title: title,
+      year: +year,
       game: game,
       tv: tv,
       tv_movie: tv_movie
@@ -33,6 +39,8 @@ fs.readFile("movie_ratings.list", "utf8", function(err, data) {
   ratings = ratings.filter(function(d) { return !d.game && !d.tv; });
   var csv = d3.csv.format(ratings);
   fs.writeFile("movie_ratings.csv", csv);
+
+  writeAPILinks(ratings.slice(0, 500));
 });
 
 function byRating(a, b) {
@@ -41,4 +49,18 @@ function byRating(a, b) {
   if (a.rating > b.rating)
     return -1;
   return 0;
+}
+
+function writeAPILinks(ratings) {
+  console.log(ratings);
+  var links = ratings.map(function(rating) {
+    return link(rating.title, rating.year);
+  });
+  fs.writeFile("links", links.join("\n"));
+}
+
+function link(title, year) {
+  return "http://www.omdbapi.com/?"
+  + "t=" + encodeURIComponent(title) + "&" +
+    "y=" + encodeURIComponent(year)
 }
